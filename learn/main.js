07323140d22364c1b20bc8a79ad4b9dbe5db1dc2
iaaing <136635217@qq.com>
2020-06-22 14:55:25 +0800

    add remote

---
+++ learn/main.js
@@ -0,0 +1,6 @@
+import Vue from "./src/core/instance"
+console.log(Vue);
+
+new Vue({
+    el: '#app'
+})
---
+++ learn/src/core/instance/index.js
@@ -0,0 +1,9 @@
+function Vue(options) {
+    this._init(options)
+}
+
+Vue.prototype._init = function (options) {
+
+}
+
+export default Vue
